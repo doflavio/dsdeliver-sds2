527534bb906f831172848d979a25f103daf7d9d6
Flávio Pereira <doflavio@gmail.com>
2021-01-15 22:49:50 -0300

    Add orders list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import { RectButton } from 'react-native-gesture-handler';
 import Header from '../Header';
+import OrdersCard from '../OrdersCard';
 
 function Orders() {
 
     return (
         <>
             <Header />
-            <View >
-                <Text > Orders</Text>
-            </View>
+            <ScrollView style={styles.container}>
+
+                <OrdersCard />
+                <OrdersCard />
+                <OrdersCard />
+                <OrdersCard />
+                <OrdersCard />
+            </ScrollView>
 
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    }
+});
 
 
 
